8e90a9ffa3a7982c3d02ec513da48d2f8d545869
Won Kyoung Park <wpark@hmc.edu>
2018-04-10 15:42:13 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-
-  get 'avatars/new' => 'avatars#new', :as => :new_avatars
 end
