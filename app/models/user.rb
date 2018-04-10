b212ba80fccfa346e9f42f225c9d0a51596d3c1e
Won Kyoung Park <wpark@hmc.edu>
2018-04-10 15:37:21 -0700

    fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
-  extend CarrierWave::Mount 
-  extend CarrierWaveDirect::Mount
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  mount_uploader :avatar, AvatarUploader
 end
