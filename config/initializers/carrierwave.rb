3f467de1bdcb015123a1d7f3129778378b1797cc
Won Kyoung Park <wpark@hmc.edu>
2018-04-10 18:34:02 -0700

    debugging carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,8 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',       # required
     :aws_access_key_id      => 'AKIAIJ5ZFBQW6OMQHBYA',       # required
     :aws_secret_access_key  => '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',       # required
-    :region                 => 'us-west-1'  # optional, defaults to 'us-east-1'
+    :region                 => 'us-west-1',  # optional, defaults to 'us-east-1'
+    :path_style             => true
   }
   case Rails.env
     when 'production'
@@ -11,8 +12,8 @@ CarrierWave.configure do |config|
       config.asset_host = 'https://s3-us-west-1.amazonaws.com/wonsample'
 
     when 'development'
-      config.fog_directory = 'dev.wonsample'
-      config.asset_host = 'https://s3-us-west-1.amazonaws.com/dev.wonsample'
+      config.fog_directory = 'wonsample'
+      config.asset_host = 'https://s3-us-west-1.amazonaws.com/wonsample'
 
     when 'test'
       config.fog_directory = 'test.wonsample'
