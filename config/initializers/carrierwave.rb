c8280f4af35cd1c904dfdf741aee5f6ab6601137
Won Kyoung Park <wpark@hmc.edu>
2018-04-11 12:02:14 -0700

    vars

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,11 +8,11 @@ CarrierWave.configure do |config|
   }
   case Rails.env
     when 'production'
-      config.fog_directory = 'wonsampleapp'
+      config.fog_directory = 'wonsample'
       config.asset_host = 'https://s3-us-west-1.amazonaws.com/#{config.fog_directory}'
 
     when 'development'
-      config.fog_directory = 'wonsampleapp'
+      config.fog_directory = 'wonsample'
       config.asset_host = 'https://s3-us-west-1.amazonaws.com/#{config.fog_directory}'
 
     when 'test'
